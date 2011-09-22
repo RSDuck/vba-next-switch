9eed5c5641d0d335baa0e76bb96de91b8bf5fa65
TwinAphex51224 <twinaphex1@gmail.com>
2011-09-22 15:53:09 +0200

    Cleaned up unused externs in Globals.h

--- trunk/src/vba/gba/Globals.h
+++ trunk/src/vba/gba/Globals.h
@@ -18,14 +18,6 @@
 
 extern reg_pair reg[45];
 extern bool ioReadable[0x400];
-extern bool N_FLAG;
-extern bool C_FLAG;
-extern bool Z_FLAG;
-extern bool V_FLAG;
-extern bool armState;
-extern bool armIrqEnable;
-extern u32 armNextPC;
-extern int armMode;
 extern u32 stop;
 extern int saveType;
 extern bool useBios;
@@ -46,31 +38,8 @@ extern bool enableRtc;
 extern bool skipSaveGameBattery; // skip battery data when reading save states
 extern bool skipSaveGameCheats;  // skip cheat list data when reading save states
 
-extern u8 *bios;
 extern u8 *rom;
-extern u8 *internalRAM;
-extern u8 *workRAM;
-extern u8 *paletteRAM;
-extern u8 *vram;
 extern u8 *pix;
-extern u8 *oam;
 extern u8 *ioMem;
-
-extern u16 DISPCNT;
-extern u16 DISPSTAT;
-extern u16 VCOUNT;
-extern u16 MOSAIC;
-extern u16 TM0D;
-extern u16 TM0CNT;
-extern u16 TM1D;
-extern u16 TM1CNT;
-extern u16 TM2D;
-extern u16 TM2CNT;
-extern u16 TM3D;
-extern u16 TM3CNT;
-extern u16 P1;
-extern u16 IE;
-extern u16 IF;
-extern u16 IME;
 
 #endif // GLOBALS_H
