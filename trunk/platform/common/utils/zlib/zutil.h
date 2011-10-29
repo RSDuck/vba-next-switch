a0d334a70f19755fb0626fb5d172d87315e53e03
Themaister <maister@archlinux.us>
2011-10-29 20:40:27 +0200

    Fix build some more.

--- trunk/platform/common/utils/zlib/zutil.h
+++ trunk/platform/common/utils/zlib/zutil.h
@@ -13,11 +13,7 @@
 #ifndef ZUTIL_H
 #define ZUTIL_H
 
-#if ((__GNUC__-0) * 10 + __GNUC_MINOR__-0 >= 33) && !defined(NO_VIZ)
-#  define ZLIB_INTERNAL __attribute__((visibility ("hidden")))
-#else
-#  define ZLIB_INTERNAL
-#endif
+#define ZLIB_INTERNAL
 
 #include "zlib.h"
 
