570579bcd13218aa5f01aefb226ca9499c33441b
Twinaphex <autechre1024@hotmail.com>
2012-03-25 23:41:46 +0200

    Clean up unused functions in util.cpp

--- src/util.cpp
+++ src/util.cpp
@@ -13,20 +13,6 @@
 
 extern bool cpuIsMultiBoot;
 
-void utilPutDword(uint8_t *p, uint32_t value)
-{
-	*p++ = value & 255;
-	*p++ = (value >> 8) & 255;
-	*p++ = (value >> 16) & 255;
-	*p = (value >> 24) & 255;
-}
-
-void utilPutWord(uint8_t *p, uint16_t value)
-{
-	*p++ = value & 255;
-	*p = (value >> 8) & 255;
-}
-
 bool utilIsGBAImage(const char * file)
 {
 	cpuIsMultiBoot = false;
@@ -52,23 +38,9 @@ bool utilIsGBAImage(const char * file)
 	return false;
 }
 
-uint32_t utilFindType(const char *file)
-{
-	uint32_t retval;
-
-	if (utilIsGBAImage(file))
-		retval = IMAGE_GBA;
-	else
-		retval = IMAGE_UNKNOWN;
-
-	return retval;
-}
-
 static int utilGetSize(int size)
 {
-	int res;
-
-	res = 1;
+	int res = 1;
 
 	while(res < size)
 		res <<= 1;
@@ -100,67 +72,6 @@ uint8_t *utilLoad(const char *file, bool (*accept)(const char *), uint8_t *data,
 	fread(image, 1, size, fp); /* read into buffer*/
 	fclose(fp);
 	return image;
-}
-
-void utilGBAFindSave(const uint8_t *data, const int size)
-{
-	uint32_t *p = (uint32_t *)data;
-	uint32_t *end = (uint32_t *)(data + size);
-	int saveType = 0;
-	int flashSize = 0x10000;
-	bool rtcFound = false;
-
-	while(p  < end) {
-		uint32_t d = READ32LE(p);
-
-		if(d == 0x52504545) {
-			if(memcmp(p, "EEPROM_", 7) == 0) {
-				if(saveType == 0)
-					saveType = 3;
-			}
-		} else if (d == 0x4D415253) {
-			if(memcmp(p, "SRAM_", 5) == 0) {
-				if(saveType == 0)
-					saveType = 1;
-			}
-		} else if (d == 0x53414C46) {
-			if(memcmp(p, "FLASH1M_", 8) == 0) {
-				if(saveType == 0) {
-					saveType = 2;
-					flashSize = 0x20000;
-				}
-			} else if(memcmp(p, "FLASH", 5) == 0) {
-				if(saveType == 0) {
-					saveType = 2;
-					flashSize = 0x10000;
-				}
-			}
-		} else if (d == 0x52494953) {
-			if(memcmp(p, "SIIRTC_V", 8) == 0)
-				rtcFound = true;
-		}
-		p++;
-	}
-	/* if no matches found, then set it to NONE*/
-	if(saveType == 0)
-		saveType = 5;
-
-	rtcEnable(rtcFound);
-	cpuSaveType = saveType;
-	flashSetSize(flashSize);
-}
-
-/* Check for existence of file.*/
-bool utilFileExists( const char *filename )
-{
-	FILE *f = fopen( filename, "r" );
-	if( f == NULL )
-		return false;
-	else
-	{
-		fclose( f );
-		return true;
-	}
 }
 
 /* Not endian safe, but VBA itself doesn't seem to care */
--- src/util.h
+++ src/util.h
@@ -15,16 +15,7 @@ typedef struct {
 } variable_desc;
 
 bool utilIsGBAImage(const char *);
-void utilStripDoubleExtension(const char *, char *);
-uint32_t utilFindType(const char *);
 uint8_t *utilLoad(const char *, bool (*)(const char*), uint8_t *, int &);
-
-void utilPutDword(uint8_t *, uint32_t);
-void utilPutWord(uint8_t *, uint16_t);
-
-void utilGBAFindSave(const uint8_t *, const int);
-
-bool utilFileExists( const char *filename );
 
 void utilWriteIntMem(uint8_t *& data, int);
 void utilWriteMem(uint8_t *& data, const void *in_data, unsigned size);
