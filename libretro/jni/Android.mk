e271f7d883039cfd06f4816a53cd1601cd7006fd
twinaphex <autechre1024@hotmail.com>
2012-10-16 15:25:48 +0200

    (Android) Fix JNI Makefile

--- libretro/jni/Android.mk
+++ libretro/jni/Android.mk
@@ -3,7 +3,7 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_MODULE    := libretro
-LOCAL_SRC_FILES    = ../src/gba.cpp ../src/memory.cpp ../src/sound.cpp ../libretro/libretro.cpp
+LOCAL_SRC_FILES    = ../../src/gba.cpp ../../src/memory.cpp ../../src/sound.cpp ../../libretro/libretro.cpp
 LOCAL_CFLAGS = -DINLINE=inline -DHAVE_STDINT_H -DHAVE_INTTYPES_H -DSPEEDHAX -DLSB_FIRST -D__LIBRETRO__
 LOCAL_C_INCLUDES = ../src
 
