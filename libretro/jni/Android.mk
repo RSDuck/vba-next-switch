d0e90355a78280c735d9db0fb6eba5e99f187dbc
twinaphex <autechre1024@hotmail.com>
2012-10-19 04:23:36 +0200

    (Android) Now compiles libraries for all Android platforms - ARM, ARMv7a, MIPS, and x86

--- libretro/jni/Android.mk
+++ libretro/jni/Android.mk
@@ -2,6 +2,18 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
+ifeq ($(TARGET_ARCH),arm)
+LOCAL_CFLAGS += -DANDROID_ARM
+endif
+
+ifeq ($(TARGET_ARCH),x86)
+LOCAL_CFLAGS +=  -DANDROID_X86
+endif
+
+ifeq ($(TARGET_ARCH),mips)
+LOCAL_CFLAGS += -DANDROID_MIPS
+endif
+
 LOCAL_MODULE    := libretro
 LOCAL_SRC_FILES    = ../../src/gba.cpp ../../src/memory.cpp ../../src/sound.cpp ../../libretro/libretro.cpp
 LOCAL_CFLAGS = -DINLINE=inline -DHAVE_STDINT_H -DHAVE_INTTYPES_H -DSPEEDHAX -DLSB_FIRST -D__LIBRETRO__
