750df378b8fd5ae0ea48eb5e2e0c0c2d1b2f0136
twinaphex <libretro@gmail.com>
2016-08-08 19:43:11 +0200

    Update

--- src/thread.cpp
+++ src/thread.cpp
@@ -23,6 +23,27 @@ void thread_run(threadfunc_t func, void* p)
 	SceUID thid = sceKernelCreateThread("my_thread", (SceKernelThreadEntry)_thread_func, 0x10000100, 0x10000, 0, 0, NULL);
 	if (thid >= 0) sceKernelStartThread(thid, sizeof(argp), &argp);
 }
+#else
+sthread_t *thid = NULL;
+
+static void _thread_func(void* p)
+{
+	void** argp = static_cast<void**>(p);
+	auto func = reinterpret_cast<threadfunc_t>(argp[0]);
+	(*func)(argp[1]);
+   pthread_detach(thid);
+}
+
+void thread_run(threadfunc_t func, void* p)
+{
+	void* argp[2];
+	argp[0] = reinterpret_cast<void*>(func);
+	argp[1] = p;
+
+   sthread_create(_thread_func, 
+	SceUID thid = sceKernelCreateThread("my_thread", (SceKernelThreadEntry)_thread_func, 0x10000100, 0x10000, 0, 0, NULL);
+	if (thid >= 0) sceKernelStartThread(thid, sizeof(argp), &argp);
+}
 #endif
 
 void thread_sleep(int ms)
